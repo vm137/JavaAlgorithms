23d56c2149d9e4c1615f982e14adc81d87a15ad7
Victor_Mikhaylov <Victor_Mikhaylov@epam.com>
2017-09-14 20:24:48 +0300

    92% percolation

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,132 +1,98 @@
-import java.lang.IllegalArgumentException;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private int MaxN, P, Q;
-    private int[][] Matrix; // 0 - closed, 1 - open, 2 - full
-    private WeightedQuickUnionUF UFstruct;
+    private final int maxN, pp, qq;
+    private int sitesOpen = 0;
+    private boolean[][] matrix; // false - closed, true - open
+    private final WeightedQuickUnionUF ufstruct;
 
     public Percolation(int n) {
         if (n <= 0) { throw new IllegalArgumentException(); }
 
-        MaxN = n;
-        P = MaxN * MaxN;
-        Q = MaxN * MaxN + 1;
-        UFstruct = new WeightedQuickUnionUF(MaxN * MaxN + 2);
-        Matrix = new int[MaxN + 1][MaxN + 1];
+        maxN = n;
+        pp = maxN * maxN;
+        qq = maxN * maxN + 1;
 
-        int row, col;
-        while (!percolates()) {
-            row = StdRandom.uniform(MaxN) + 1;
-            col = StdRandom.uniform(MaxN) + 1;
-
-            if (!isOpen(row, col)) {
-                open(row, col);
-            }
-        }
+        ufstruct = new WeightedQuickUnionUF(maxN * maxN + 2);
+        matrix = new boolean[maxN + 1][maxN + 1];
     }
 
     public void open(int row, int col) {
         checkIfValid(row, col);
 
-        Matrix[row][col] = 1;
+        if (!isOpen(row, col)) {
+            matrix[row][col] = true;
+            sitesOpen++;
+        }
 
-        // P
+        // pp
         if (row == 1) {
-            UFstruct.union(rowColTo1D(row, col), P);
+            ufstruct.union(rowColTo1D(row, col), pp);
         }
 
         // top
         if (isValid(row - 1, col) && isOpen(row - 1, col)) {
-            UFstruct.union(rowColTo1D(row, col), rowColTo1D(row - 1, col));
+            ufstruct.union(rowColTo1D(row, col), rowColTo1D(row - 1, col));
         }
 
         // right
         if (isValid(row, col + 1) && isOpen(row, col + 1)) {
-            UFstruct.union(rowColTo1D(row, col), rowColTo1D(row, col + 1));
+            ufstruct.union(rowColTo1D(row, col), rowColTo1D(row, col + 1));
         }
 
         // bottom
         if (isValid(row + 1, col) && isOpen(row + 1, col)) {
-            UFstruct.union(rowColTo1D(row, col), rowColTo1D(row + 1, col));
+            ufstruct.union(rowColTo1D(row, col), rowColTo1D(row + 1, col));
         }
 
         // left
         if (isValid(row, col - 1) && isOpen(row, col - 1)) {
-            UFstruct.union(rowColTo1D(row, col), rowColTo1D(row, col - 1));
+            ufstruct.union(rowColTo1D(row, col), rowColTo1D(row, col - 1));
         }
 
-        // Q
-        if (row == MaxN) {
-            UFstruct.union(rowColTo1D(row, col), Q);
+        // qq
+        if (row == maxN) {
+            ufstruct.union(rowColTo1D(row, col), qq);
         }
 
-        makeFull(row, col);
     }
 
-    private void makeFull(int row, int col) {
-        if (row == 1
-                || isValid(row - 1, col) && isFull(row - 1, col)
-                || isValid(row, col - 1) && isFull(row, col - 1)
-                || isValid(row + 1, col) && isFull(row + 1, col)
-                || isValid(row, col + 1) && isFull(row, col + 1)) {
-
-            Matrix[row][col] = 2;
-
-            if (isValid(row - 1, col) && isOpen(row - 1, col) && !isFull(row - 1, col)) {
-                makeFull(row - 1, col);
-            }
-
-            if (isValid(row, col - 1) && isOpen(row, col - 1) && !isFull(row, col - 1)) {
-                makeFull(row, col - 1);
-            }
-
-            if (isValid(row + 1, col) && isOpen(row + 1, col) && !isFull(row + 1, col)) {
-                makeFull(row + 1, col);
-            }
-
-            if (isValid(row, col + 1) && isOpen(row, col + 1) && !isFull(row, col + 1)) {
-                makeFull(row, col + 1);
-            }
-        }
-    }
-
-    private boolean isValid (int row, int col) {
-        return (row >= 1 && row <= MaxN) && (col >= 1 && col <= MaxN);
+    private boolean isValid(int row, int col) {
+        return (row >= 1 && row <= maxN) && (col >= 1 && col <= maxN);
     }
 
     private void checkIfValid(int row, int col) {
-        if ((row < 1 || row > MaxN) || (col < 1 || col > MaxN))
+        if ((row < 1 || row > maxN) || (col < 1 || col > maxN))
             throw new IllegalArgumentException();
     }
 
-    private int rowColTo1D (int row, int col) {
-        return (row - 1) * MaxN + (col - 1 );
+    private int rowColTo1D(int row, int col) {
+        return (row - 1) * maxN + (col - 1);
     }
 
     public boolean isOpen(int row, int col) {
         checkIfValid(row, col);
-        return (Matrix[row][col] >= 1);
+        return matrix[row][col];
     }
 
     public boolean isFull(int row, int col) {
         checkIfValid(row, col);
-        return (Matrix[row][col] == 2);
+        return ufstruct.connected(pp, rowColTo1D(row, col));
     }
 
     public int numberOfOpenSites() {
-        return MaxN * MaxN - (UFstruct.count() - 2);
+        return sitesOpen;
     }
 
     public boolean percolates() {
-        return UFstruct.connected(P, Q);
+        return ufstruct.connected(pp, qq);
     }
 
-    public void visualizeMatrix() {
-        for (int row = 1; row <= MaxN; row++) {
-            for (int col = 1; col <= MaxN; col++) {
-                System.out.print(Matrix[row][col]);
+    private void visualizeMatrix() {
+        for (int row = 1; row <= maxN; row++) {
+            for (int col = 1; col <= maxN; col++) {
+                System.out.print(matrix[row][col] ? 1 : 0);
             }
             System.out.print("\n");
         }
@@ -134,9 +100,19 @@ public class Percolation {
     }
 
     public static void main(String[] args) {
-        int n = 10;
+        int n = 5;
         Percolation p = new Percolation(n);
         System.out.println("Percolation constructor (x" + n + ")\n");
+
+        int row, col;
+        while (!p.percolates()) {
+            row = StdRandom.uniform(n) + 1;
+            col = StdRandom.uniform(n) + 1;
+
+            if (!p.isOpen(row, col)) {
+                p.open(row, col);
+            }
+        }
 
         p.visualizeMatrix();
         System.out.println("percolates with: " + p.numberOfOpenSites() + " sites of " + n * n + " (" + ((p.numberOfOpenSites()) / (n * n * 1.0)) + ")");
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,22 +1,30 @@
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private double[] thresholds;
-    private double mean, dev;
+    private final double[] thresholds;
+    private double mean, devt;
 
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) { throw new IllegalArgumentException(); }
-        // System.out.printf("%dx%d %d times%n%n", n, n, trials);
 
+        int row, col;
         thresholds = new double[trials];
+
         for (int i = 0; i < trials; i++) {
             Percolation p = new Percolation(n);
-            thresholds[i] = (double)p.numberOfOpenSites() / (n * n);
-        }
 
-        System.out.printf("mean = %g %n", mean());
-        System.out.printf("stddev = %g %n", stddev());
-        System.out.printf("95%% confidence interval = [%g, %g]", confidenceLo(), confidenceHi());
+            while (!p.percolates()) {
+                row = StdRandom.uniform(n) + 1;
+                col = StdRandom.uniform(n) + 1;
+
+                if (!p.isOpen(row, col)) {
+                    p.open(row, col);
+                }
+            }
+
+            thresholds[i] = p.numberOfOpenSites() / (n * n * 1.0);
+        }
     }
 
     public double mean() {
@@ -25,23 +33,25 @@ public class PercolationStats {
     }
 
     public double stddev() {
-        dev = StdStats.stddev(thresholds);
-        return dev;
+        devt = StdStats.stddev(thresholds);
+        return devt;
     }
 
     public double confidenceLo() {
-        return mean - .95 * dev;
+        return mean - (1.96 * devt / Math.sqrt(thresholds.length));
     }
 
     public double confidenceHi() {
-        return mean + .95 * dev;
+        return mean + (1.96 * devt / Math.sqrt(thresholds.length));
     }
 
     public static void main(String[] args) {
         if (args.length != 2) { throw new IllegalArgumentException("incorrect input arguments for main()"); }
-        int n = Integer.parseInt(args[0]);
-        int t = Integer.parseInt(args[1]);
 
-        new PercolationStats(n, t);
+        PercolationStats ps = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+
+        System.out.printf("mean = %g %n", ps.mean());
+        System.out.printf("stddev = %g %n", ps.stddev());
+        System.out.printf("95%% confidence interval = [%g, %g]", ps.confidenceLo(), ps.confidenceHi());
     }
 }
